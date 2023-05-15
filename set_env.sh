43e5278316d93f811fd26d2127a2ccae3b344443
HuynhNguyen9797 <HuynhNguyen9797@outlook.com>
2023-05-15 22:41:15 +0700

    HuynhNN-develop

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=huynhnn2
+export POSTGRES_PASSWORD=HuynhNguyen1504!\(\(\&
+export POSTGRES_HOST=huynhnn-project3.cywlkxt0omqb.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=huynhnn2-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
